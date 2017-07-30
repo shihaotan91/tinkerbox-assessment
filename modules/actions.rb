867f4a0a2c61ae58815ca91ddf4e15a1a0ab494e
Shi Hao Tan <shihaotan91@gmail.com>
2017-07-30 22:07:22 +0800

    Refactor code to provide proper variable scoping and make methods name more explicit

--- modules/actions.rb
+++ modules/actions.rb
@@ -5,16 +5,16 @@ include FindCSV
 
 module Actions
 
-  complete = "Mark as complete"
-  incomplete = "Mark as incomplete"
-  doing = "Mark as doing"
+  COMPLETE = "Mark as complete"
+  INCOMPLETE = "Mark as incomplete"
+  DOING = "Mark as doing"
 
-  $actions_array = [complete, incomplete, doing]
+  @@actions_array = [COMPLETE, INCOMPLETE, DOING]
 
   def show_actions
 
     puts "Actions\n-------"
-    $actions_array.each_with_index do |text, i|
+    @@actions_array.each_with_index do |text, i|
       puts "#{i+1}. #{text}"
     end
     puts "Type 'delete' to delete selected item"
@@ -22,18 +22,26 @@ module Actions
 
   end
 
+  def assign_status
+    system("sed -i '' '#{@selection_number},#{@selection_number} s/$/ [#{@@actions_array[@@action_number-1].split.last.capitalize}]/' #{@input}")
+  end
+
+  def delete_item
+    system("sed -i '' #{@selection_number}d #{@input}")
+    puts "Item #{@selection_number} deleted"
+  end
+
   def perform_action
 
     show_actions()
 
     action = gets.chomp.downcase
-    action_number = action.to_i
+    @@action_number = action.to_i
 
-    if (action_number > 0 && action_number <= $actions_array.size + 1 )
-      system("sed -i '' '#{@selection_number},#{@selection_number} s/$/ [#{$actions_array[action_number-1].split.last.capitalize}]/' #{@input}")
+    if (@@action_number > 0 && @@action_number <= @@actions_array.size + 1 )
+      assign_status()
     elsif (action == 'delete')
-      system("sed -i '' #{@selection_number}d #{@input}")
-      puts "Item #{@selection_number} deleted"
+      delete_item()
     elsif (action == 'back')
       select_item()
     else
